7e111f3eddbe6f59382cc6b57d20dc139be6e045
Vinicius Rayol <102189417+Viinee1@users.noreply.github.com>
2024-06-11 21:02:19 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import theme from '@theme/index'
+import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading'
+
+
+export default function App() {
+  const [fontsLoaded]  = useFonts({ Roboto_400Regular, Roboto_700Bold});
+  
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Groups/> : <Loading />}
+    </ThemeProvider>
+  );
+}
+
+
+
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,21 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+
+import { Container } from './styles';
+
+
+
+export function Groups(){
+    return(
+        <Container>
+            <Header/>
+            <Highlight
+            title='Turmas'
+            subtitle='jogue com a sua turma'
+            />
+
+            <GroupCard title='Galera do Ignite'/>
+        </Container>
+    );
+}
