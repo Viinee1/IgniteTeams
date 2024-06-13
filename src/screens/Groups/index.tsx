a7911f796cf9fc52f9f709af997d23bc592b10f2
Vinicius Rayol <102189417+Viinee1@users.noreply.github.com>
2024-06-13 17:37:20 -0300

    Add files via upload

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
 
 
 export function Groups(){
+    const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
+
+
     return(
         <Container>
             <Header/>
@@ -15,7 +23,24 @@ export function Groups(){
             subtitle='jogue com a sua turma'
             />
 
-            <GroupCard title='Galera do Ignite'/>
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                     title={item}
+                     />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty 
+                        message='Que tal cadastrar a primeira turma?'
+                    />
+                )}        
+            />
+            <Button
+            title='Criar nova turma'
+            />
         </Container>
     );
 }
