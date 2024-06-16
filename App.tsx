d3bbe7913a5e1ba7682caad54b1d6abe7310d452
Vinicius Rayol <102189417+Viinee1@users.noreply.github.com>
2024-06-16 00:41:06 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import theme from '@theme/index'
+
+
 import { Groups } from '@screens/Groups';
+
 import { Loading } from '@components/Loading'
 
 
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,78 @@
+import { FlatList } from 'react-native';
+import { Highlight } from '@components/Highlight';
+import { Header } from '@components/Header';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
+import { useState } from 'react';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+export function Players() {
+    const [team, setTeam] = useState('Time A');
+    const [players, setPlayers] = useState([]);
+
+    return (
+        <Container>
+            <Header showBackButton/>
+            <Highlight
+                title='Nome da turma'
+                subtitle='adicione a galera e separe os times'
+            />
+
+            <Form>
+
+            <Input
+                placeholder='Nome da pessoa'
+                autoCorrect={false}
+            />
+            <ButtonIcon icon='add' />
+            </Form>
+
+            <HeaderList>
+            <FlatList
+                data={['Time A', 'Time B']}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Filter
+                        title={item}
+                        isActive={item === team}
+                        onPress={() => setTeam(item)}
+                    />
+                )}
+                horizontal
+            />
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+            </HeaderList>
+
+            <FlatList
+            data={players}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+                <PlayerCard
+                 name={item}
+                 onRemove={() => { } } />
+            )}
+            ListEmptyComponent={() => (
+                <ListEmpty
+                    message='Não há pessoas nesse time'
+                />
+            )}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
+            />
+
+            <Button
+                title="Remover Turma"
+                type="SECONDARY"
+            />
+
+
+        </Container>
+    );
+
+}
